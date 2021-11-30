d6020431c7d69a37462428c9a09e875fbd6e9790
HenryAprendo <hjsmipuc@gmail.com>
2021-11-30 17:02:00 -0500

    Agrupe el codigo en funciones y utilice un formulario para recibir datos y calcular lalas operaciones desde el usuario

--- figurasGeometricas/figuras.js
+++ figurasGeometricas/figuras.js
@@ -1,1 +1,57 @@
-console.log("Start Course JS Practice");
+//Codigo del cuadrado
+function perimetroCuadrado(lado){
+    return lado * 4;
+}
+
+function areaCuadrado(lado){
+    return lado * lado;
+} 
+
+
+//Codigo del triangulo
+function perimetroTriangulo(lado1, lado2, base){
+    return lado1 + lado2 + base;
+}
+
+function areatriangulo(base, altura){
+    return (base * altura) / 2;
+} 
+
+
+//Codigo del circulo
+
+//PI
+const PI = Math.PI;
+
+//circunferencia
+function diametroCirculo(radio){
+    return radio * 2;
+}
+
+function perimetroCirculo(radio){
+    const diametro = diametroCirculo(radio);
+    return diametro * PI;
+} 
+
+function areaCirculo(radio){
+    return (radio * radio) * PI;
+} 
+
+//Interactuando con el HTML
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;    
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+
+function calcularAreaCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;    
+    const area = areaCuadrado(value);
+    alert(area);
+}
+
+
+
+
