a1148c02432adab3abdf34434eb982956668b2c2
HenryAprendo <hjsmipuc@gmail.com>
2021-12-03 18:01:38 -0500

    Inicio del reto <descuento con cupón>

---
+++ retosDelCurso/bonoDescuento/cupon.js
@@ -0,0 +1,17 @@
+var cuponDiezPorCiento = "cupon#10%";
+var cuponVeintePorCiento = "cupon#20%";
+var cuponCincuentaPorCiento = "cupon#50%";
+
+const precioProducto = 5000;
+const porcentajeDescuento = 20;
+
+const descuento = 100 - porcentajeDescuento;
+const precioConDescuento = (precioProducto * descuento) / 100;
+
+console.log(precioConDescuento);
+
+
+
+
+
+
