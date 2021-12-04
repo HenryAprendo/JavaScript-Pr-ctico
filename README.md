a1679078c8c6f53b71f5c35a57260864f384f23e
HenryAprendo <hjsmipuc@gmail.com>
2021-12-04 18:21:22 -0500

    programación del codigo JavaScript del reto cupón de descuento, pruebas de funcionamiento correctas

--- README.md
+++ README.md
@@ -9,3 +9,16 @@
 - Quinto paso: Dar estilos con CSS
 - Sexto paso: Desplegar en Github Page
 - Septimo paso: Nueva versión y mantenimiento.
+
+## Taller #2 : Precio con descuento
+- Primer paso: Definir formulas
+- Segundo paso: Codificar en JS
+- Tercer paso: Implementar funciones
+- Cuarto paso: Integrar JS con HTML
+- Quinto paso: Dar estilos con CSS
+- Sexto paso: Desplegar en Github Page
+- Septimo paso: Nueva versión y mantenimiento.
+
+# Retos 
+1. Terminación del primer taller y triangulo isosceles
+2. Cupon con descuento
--- retosDelCurso/bonoDescuento/cupon.js
+++ retosDelCurso/bonoDescuento/cupon.js
@@ -1,14 +1,77 @@
+//Descuento por cupon
 var cuponDiezPorCiento = "cupon#10%";
 var cuponVeintePorCiento = "cupon#20%";
 var cuponCincuentaPorCiento = "cupon#50%";
 
-const precioProducto = 5000;
-const porcentajeDescuento = 20;
 
-const descuento = 100 - porcentajeDescuento;
-const precioConDescuento = (precioProducto * descuento) / 100;
+//Creación de funciones.
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajeDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajeDescuento) / 100;
+    return precioConDescuento;
+}
 
-console.log(precioConDescuento);
+function calcularDiezPorciento(precio, descuento){
+    const descuentoDiez = (precio * 10) / 100;
+    const price = calcularPrecioConDescuento(precio, descuento);
+    const valorTotal =  price - descuentoDiez;
+    return valorTotal;
+}
+
+function calcularVeintePorciento(precio, descuento){
+    const descuentoVeinte = (precio * 20) / 100;
+    const price = calcularPrecioConDescuento(precio, descuento);
+    const valorTotal = price - descuentoVeinte;
+    return valorTotal;
+}
+
+function calcularCincuentaPorciento(precio, descuento){
+    const descuentoCincuenta = (precio * 50) / 100;
+    const price = calcularPrecioConDescuento(precio, descuento);
+    const valorTotal = price - descuentoCincuenta;
+    return valorTotal;
+}
+
+//Interactuando con el html
+
+function calcularPrecioProducto(){
+    const price = document.getElementById("InputPrice");
+    const valuePrice = price.value;
+    const discount = document.getElementById("Inputdiscount");
+    const valueDiscount = discount.value;
+    const cupon = document.getElementById("cupon");
+    const inputText = cupon.value;
+    var precioProducto = 0;  
+
+    if(inputText === cuponDiezPorCiento ){
+       precioProducto = calcularDiezPorciento(valuePrice, valueDiscount);
+    }else if(inputText === cuponVeintePorCiento){
+       precioProducto = calcularVeintePorciento(valuePrice, valueDiscount);
+    }else if(inputText === cuponCincuentaPorCiento){
+       precioProducto = calcularCincuentaPorciento(valuePrice, valueDiscount);
+    }else{
+       precioProducto = calcularPrecioConDescuento(valuePrice, valueDiscount);
+    }
+
+    const resultado = document.getElementById("resultado");
+    resultado.innerText = `El costo del producto con descuento son: ${precioProducto} dolares`;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
