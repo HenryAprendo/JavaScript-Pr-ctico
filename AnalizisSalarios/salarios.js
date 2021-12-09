64b2f7851181435620ca2dcda1d60575ca6312ea
HenryAprendo <hjsmipuc@gmail.com>
2021-12-09 16:59:47 -0500

    Clase analisis de salarios - practicando lo aprendido

---
+++ AnalizisSalarios/salarios.js
@@ -0,0 +1,104 @@
+const colombia = [];
+
+colombia.push({
+    name: "Henry",
+    salary: 1000
+});
+
+colombia.push({
+    name: "Andres",
+    salary: 1500
+});
+
+colombia.push({
+    name: "Martha",
+    salary: 1300
+});
+
+colombia.push({
+    name: "Ana",
+    salary: 1400
+});
+
+colombia.push({
+    name: "Maida",
+    salary: 1700
+});
+
+colombia.push({
+    name: "Yaniris",
+    salary: 1500
+});
+
+
+colombia.push({
+    name: "Yeinis",
+    salary: 1000
+});
+
+colombia.push({
+    name: "Juan",
+    salary: 1800
+});
+
+colombia.push({
+    name: "Carlos",
+    salary: 1200
+});
+
+colombia.push({
+    name: "Emilis",
+    salary: 900
+});
+
+colombia.push({
+    name: "Osvaldo",
+    salary: 800
+});
+
+colombia.push({
+    name: "Mileidis",
+    salary: 700
+});
+
+colombia.push({
+    name: "Rosa",
+    salary: 1000
+});
+
+colombia.push({
+    name: "Luis",
+    salary: 1300
+});
+
+colombia.push({
+    name: "Roman",
+    salary: 1300
+});
+
+colombia.push({
+    name: "Freddy",
+    salary: 1600
+});
+
+colombia.push({
+    name: "Marina",
+    salary: 1100
+});
+
+colombia.push({
+    name: "Ibis",
+    salary: 1400
+});
+
+colombia.push({
+    name: "Sergio",
+    salary: 500
+});
+
+colombia.push({
+    name: "Jerson",
+    salary: 800
+});
+
+console.log(colombia);
